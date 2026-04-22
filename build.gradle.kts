6058dfee08112010e9ceb0b281679293faa7899e
Saumya <mishrasaumya960@gmail.com>
2026-04-22 16:49:18 +0530

    Fix buildscript kts syntax error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-buildscript {
-    ext {
-        set("compose_version", "1.5.1")
-        set("kotlin_version", "1.9.0")
-        set("room_version", "2.6.1")
-    }
-}
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
